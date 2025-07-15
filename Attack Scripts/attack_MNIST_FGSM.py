Got a crapton of models, adapted dataset preprocessing to include proper normalization and reshaping procedures. Ran into strange tensorflow int overflow bug.

Attack Scripts/attack_MNIST_FGSM.py
@@ -21,21 +21,13 @@ from sklearn.model_selection import train_test_split
 # import Helpers.RDSA_Helpers as RDSA_Help
 import Attacks.constrained_FGSM as cFGSM
 
+# Input file paths
+datasetPath = "Datasets/MNIST/train_data.npy"
+labelsPath = "Datasets/MNIST/train_target.npy"
+modelPath = "Models/MNIST/base_model.keras"
 
-
-
-trainTestSplitSeed = 42
-
-evaluateBase = False
-
-dataset_name = "mnist_784"
-dataset_path = "Datasets/MNIST/MNIST784_data.npy"
-labels_path = "Datasets/MNIST/MNIST784_target.npy"
-force_download = False
-save_locally = True
-
-model_path = "Models/MNIST/best_model.keras"
-results_path = "Results/TestBaseModel/"
+# Output file path for fooling success indicators 
+successPath = "Results/MNIST/FGSM_fooling_success.npy"
 
 lossObject = keras.losses.CategoricalCrossentropy()
 epsilon = 0.1
@@ -43,44 +35,28 @@ epsilon = 0.1
 n = 100
 workercount = 8
 chunksize = 16
-adversaryFolder = "Datasets/MNIST/"
 
 if __name__ == "__main__":
 
     # Load dataset
-    # If the dataset is saved locally, just use that instead of re-downloading. This assumes that it is already properly normaized and categorized,
-    if os.path.isfile(dataset_path) and os.path.isfile(labels_path) and not force_download:
+    # If the dataset is saved locally, just use that instead of re-downloading. This assumes that it is already properly normalized and categorized.
+    if os.path.isfile(datasetPath) and os.path.isfile(labelsPath):
         print("Found local dataset and labels.")
-        X = np.load(dataset_path, allow_pickle=True)
-        Y = np.load(labels_path, allow_pickle=True)
+        data = np.load(datasetPath, allow_pickle=True)
+        labels = np.load(labelsPath, allow_pickle=True)
     else:
-        print("Did not find dataset. Make sure it is downloaded and properly preprocessed.")
-        # baseDataset = fetch_openml(dataset_name, as_frame = False, parser="liac-arff")
-
-        # # Extract and normalize the examples
-        # X = StandardScaler().fit_transform(baseDataset.data.astype(np.float32))
-        # # Encode the labels as one-hot vectors
-        # Y = to_categorical(baseDataset.target.astype(int))
-
-        # if save_locally:
-        #     print("Saving dataset.")
-        #     np.save(dataset_path, X)
-        #     np.save(labels_path, Y)
-
-    # Perform train-test-split.
-    # We're using a pre-trained model here, which should be trained on the same split to avoid evaluating on training examples 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=trainTestSplitSeed)
+        print("Did not find dataset or labels. Make sure it is downloaded and properly preprocessed using the given helper script.")
+        quit()
 
     # Load pre-trained Model
-    model = keras.models.load_model(model_path)
-
-
+    model = keras.models.load_model(modelPath)
+    model.summary()
 
     # Perform parallel FGSM (on first n testing samples)
     adversaries, newLabels, success = cFGSM.parallel_constrained_FGSM(
         model = model,
-        dataset = X_test[:n],
-        labels = Y_test[:n],
+        dataset = data[:n],
+        labels = labels[:n],
         lossObject = lossObject,
         epsilon = epsilon,
         constrainer = None,
@@ -90,6 +66,6 @@ if __name__ == "__main__":
 
     print("saving")
 
-    np.save(adversaryFolder + "MNIST784_adv_FGSM_data.npy", adversaries)
-    np.save(adversaryFolder + "MNIST784_adv_FGSM_labels.npy", newLabels)
-    np.save(adversaryFolder + "MNIST784_adv_FGSM_indicators.npy", success)
+    np.save(datasetPath.replace(".npy", "_adv.npy"), adversaries)
+    np.save(labelsPath.replace(".npy", "_adv.npy"), newLabels)
+    np.save(successPath, success)

Attack Scripts/attack_MNIST_RDSA.py
@@ -58,22 +58,7 @@ if __name__ == "__main__":
         X = np.load(dataset_path, allow_pickle=True)
         Y = np.load(labels_path, allow_pickle=True)
     else:
-        print("Downloading dataset.")
-        baseDataset = fetch_openml(dataset_name, as_frame = False, parser="liac-arff")
-
-        # Extract and normalize the examples
-        X = StandardScaler().fit_transform(baseDataset.data.astype(np.float32))
-        # Encode the labels as one-hot vectors
-        Y = to_categorical(baseDataset.target.astype(int))
-
-        if save_locally:
-            print("Saving dataset.")
-            np.save(dataset_path, X)
-            np.save(labels_path, Y)
-
-    # Perform train-test-split.
-    # We're using a pre-trained model here, which should be trained on the same split to avoid evaluating on training examples 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=trainTestSplitSeed)
+        print("Did not find dataset. Make sure it is downloaded and properly preprocessed.")
 
     # Load pre-trained Model
     model = keras.models.load_model(model_path)

Helpers/TopoDNN/topodnnpreprocessing.py
@@ -2,6 +2,7 @@
 # The original can be found  
 # Modified by Maximilian Miles to fit the requirements of this project.
 
+import warnings
 import pandas as pd
 import math 
 import numpy as np
@@ -9,6 +10,7 @@ import glob
 import sklearn
 import sys, os
 from sklearn.metrics import roc_curve
+import tqdm
 
 def get_pt_eta_phi_v(px, py, pz):
     '''Provides pt, eta, and phi given px, py, pz'''
@@ -40,93 +42,113 @@ def rotate_v(py, pz, angle):
     pzz = pz * np.cos(angle) + py * np.sin(angle)
     return pyy, pzz
 
-# Train data
-# Read in hdf5 files
-# Note: you will need to repeat this processing for input_filenames train, test and val
-input_filename = sys.argv[1]
-assert os.path.exists(input_filename)
-assert input_filename.endswith(".h5")
-store = pd.HDFStore(input_filename)
-df = store.select("table")
+# Read in a given file, extract and preprocess the 30 highest-pT constituents and save a numpy array encoding the events and the targets. 
+def topodnn_preprocess(input_filename):
+    assert os.path.exists(input_filename)
+    assert input_filename.endswith(".h5")
+    store = pd.HDFStore(input_filename)
+    df = store.select("table")
 
-n_constits = 30 # use only 30 highest pt jet constituents 
-df_pt_eta_phi = pd.DataFrame()
+    n_constits = 30 # use only 30 highest pt jet constituents
+    df_pt_eta_phi = pd.DataFrame()
 
-for j in range(n_constits):
-    i = str(j)
-    print("Processing constituent #"+str(i))
-    px = np.array(df["PX_"+i][0:])
-    py = np.array(df["PY_"+i][0:])
-    pz = np.array(df["PZ_"+i][0:])
-    pt,eta,phi = get_pt_eta_phi_v(px,py,pz)
-    df_pt_eta_phi_mini = pd.DataFrame(np.stack([pt,eta,phi]).T,columns = ["pt_"+i,"eta_"+i,"phi_"+i])
-    df_pt_eta_phi = pd.concat([df_pt_eta_phi,df_pt_eta_phi_mini], axis=1, sort=False)
+    print("Reading constituents")
+    for j in tqdm.tqdm(range(n_constits)):
+        i = str(j)
+        px = np.array(df["PX_"+i][0:])
+        py = np.array(df["PY_"+i][0:])
+        pz = np.array(df["PZ_"+i][0:])
+        pt,eta,phi = get_pt_eta_phi_v(px,py,pz)
+        df_pt_eta_phi_mini = pd.DataFrame(np.stack([pt,eta,phi]).T,columns = ["pt_"+i,"eta_"+i,"phi_"+i])
+        df_pt_eta_phi = pd.concat([df_pt_eta_phi,df_pt_eta_phi_mini], axis=1, sort=False)
 
-df = df.reset_index()
-df_pt_eta_phi["is_signal_new"] = df["is_signal_new"]
-del df
+    df = df.reset_index()
+    df_pt_eta_phi["is_signal_new"] = df["is_signal_new"]
+    del df
 
-MIN_PT = 0.0
-MAX_PT = 1679.1593231 # hard-coded from paper
+    MIN_PT = 0.0
+    MAX_PT = 1679.1593231 # hard-coded from paper
 
-df_pt_eta_phi_scaled = df_pt_eta_phi.copy()
-pt_cols = [col for col in df_pt_eta_phi.columns if 'pt' in col]
-eta_cols = [col for col in df_pt_eta_phi.columns if 'eta' in col]
-phi_cols = [col for col in df_pt_eta_phi.columns if 'phi' in col]
-df_pt_eta_phi_scaled[pt_cols]= (df_pt_eta_phi.filter(regex='pt')-MIN_PT)/(MAX_PT-MIN_PT)
+    df_pt_eta_phi_scaled = df_pt_eta_phi.copy()
+    pt_cols = [col for col in df_pt_eta_phi.columns if 'pt' in col]
+    eta_cols = [col for col in df_pt_eta_phi.columns if 'eta' in col]
+    phi_cols = [col for col in df_pt_eta_phi.columns if 'phi' in col]
+    df_pt_eta_phi_scaled[pt_cols]= (df_pt_eta_phi.filter(regex='pt')-MIN_PT)/(MAX_PT-MIN_PT)
 
-df_pt_eta_phi_translated = df_pt_eta_phi_scaled.copy()
-df_pt_eta_phi_translated[phi_cols]= df_pt_eta_phi_scaled.filter(regex='phi')-MIN_PT
+    df_pt_eta_phi_translated = df_pt_eta_phi_scaled.copy()
+    df_pt_eta_phi_translated[phi_cols]= df_pt_eta_phi_scaled.filter(regex='phi')-MIN_PT
 
-# Translate in eta by the eta of the first constituent
-eta_shift = df_pt_eta_phi_scaled['eta_0']
-df_pt_eta_phi_translated[eta_cols] = df_pt_eta_phi_scaled.filter(regex='eta')-np.tile(eta_shift, [n_constits,1]).T
+    # Translate in eta by the eta of the first constituent
+    eta_shift = df_pt_eta_phi_scaled['eta_0']
+    df_pt_eta_phi_translated[eta_cols] = df_pt_eta_phi_scaled.filter(regex='eta')-np.tile(eta_shift, [n_constits,1]).T
 
-# Translate in phi 
-phi_shift = np.array(df_pt_eta_phi_scaled['phi_0'])
-x = np.where(phi_shift < -math.pi)
-y = np.where(phi_shift >= math.pi)
-if x:
-    phi_shift[x] = phi_shift[x]+2*math.pi
-if y:
-    phi_shift[y] = phi_shift[y]-2*math.pi
+    # Translate in phi 
+    phi_shift = np.array(df_pt_eta_phi_scaled['phi_0'])
+    x = np.where(phi_shift < -math.pi)
+    y = np.where(phi_shift >= math.pi)
+    if x:
+        phi_shift[x] = phi_shift[x]+2*math.pi
+    if y:
+        phi_shift[y] = phi_shift[y]-2*math.pi
 
-df_pt_eta_phi_translated[phi_cols] = df_pt_eta_phi_scaled.filter(regex='phi')-np.tile(phi_shift, [n_constits,1]).T
-del df_pt_eta_phi_scaled
+    df_pt_eta_phi_translated[phi_cols] = df_pt_eta_phi_scaled.filter(regex='phi')-np.tile(phi_shift, [n_constits,1]).T
+    del df_pt_eta_phi_scaled
 
-df_pt_eta_phi_rotated = pd.DataFrame()
+    df_pt_eta_phi_rotated = pd.DataFrame()
 
-# Calculate theta from second jet constituents
-pt =  np.array(df_pt_eta_phi_translated['pt_1'])[0:]
-eta = np.array(df_pt_eta_phi_translated['eta_1'])[0:]
-phi = np.array(df_pt_eta_phi_translated['phi_1'])[0:]
-px, py, pz = get_px_py_pz_v(pt,eta,phi)
-theta = np.arctan2(py, pz) + math.pi/2
-
-for j in range(n_constits):
-    i = str(j)
-    pt =  np.array(df_pt_eta_phi_translated['pt_'+i])[0:]
-    eta = np.array(df_pt_eta_phi_translated['eta_'+i])[0:]
-    phi = np.array(df_pt_eta_phi_translated['phi_'+i])[0:]
-     
-    # Rotate by theta
+    # Calculate theta from second jet constituents
+    pt =  np.array(df_pt_eta_phi_translated['pt_1'])[0:]
+    eta = np.array(df_pt_eta_phi_translated['eta_1'])[0:]
+    phi = np.array(df_pt_eta_phi_translated['phi_1'])[0:]
     px, py, pz = get_px_py_pz_v(pt,eta,phi)
-    py, pz = rotate_v(py, pz, theta)
-    pt, eta, phi = get_pt_eta_phi_v(px, py, pz)
-    df_pt_eta_phi_mini = pd.DataFrame(np.stack([pt,eta,phi]).T,columns = ["pt_"+i,"eta_"+i,"phi_"+i])
-    df_pt_eta_phi_rotated = pd.concat([df_pt_eta_phi_rotated,df_pt_eta_phi_mini], axis=1, sort=False)
+    theta = np.arctan2(py, pz) + math.pi/2
 
-df_pt_eta_phi_rotated["is_signal_new"] = df_pt_eta_phi_translated["is_signal_new"]
-del df_pt_eta_phi_translated
+    print("Processing constituents.")
+    for j in tqdm.tqdm(range(n_constits)):
+        i = str(j)
+        pt =  np.array(df_pt_eta_phi_translated['pt_'+i])[0:]
+        eta = np.array(df_pt_eta_phi_translated['eta_'+i])[0:]
+        phi = np.array(df_pt_eta_phi_translated['phi_'+i])[0:]
+        
+        # Rotate by theta
+        px, py, pz = get_px_py_pz_v(pt,eta,phi)
+        py, pz = rotate_v(py, pz, theta)
+        pt, eta, phi = get_pt_eta_phi_v(px, py, pz)
+        df_pt_eta_phi_mini = pd.DataFrame(np.stack([pt,eta,phi]).T,columns = ["pt_"+i,"eta_"+i,"phi_"+i])
+        df_pt_eta_phi_rotated = pd.concat([df_pt_eta_phi_rotated,df_pt_eta_phi_mini], axis=1, sort=False)
 
-# Move average jet pt to right hand plane
-eta_times_pt = np.multiply(df_pt_eta_phi_rotated.filter(regex='pt'),df_pt_eta_phi_rotated.filter(regex='eta'))
-centre = np.sum(eta_times_pt,axis=1)
-x = np.where(centre < 0)
-df_pt_eta_phi_flipped = df_pt_eta_phi_rotated.copy()
-for col in eta_cols: # funny syntax. 
-    df_pt_eta_phi_flipped[col].loc[x] = -1.0*df_pt_eta_phi_flipped[col].loc[x]
+    df_pt_eta_phi_rotated["is_signal_new"] = df_pt_eta_phi_translated["is_signal_new"]
+    del df_pt_eta_phi_translated
 
-del df_pt_eta_phi_rotated
+    # Move average jet pt to right hand plane by flipping eta if the average eta*pT for a jet is negative.
+    eta_times_pt = np.multiply(df_pt_eta_phi_rotated.filter(regex='pt'),df_pt_eta_phi_rotated.filter(regex='eta'))
+    centre = np.sum(eta_times_pt,axis=1)
+    events_to_flip = np.where(centre < 0)[0]
+    df_pt_eta_phi_flipped = df_pt_eta_phi_rotated.copy()
 
-df_pt_eta_phi_flipped.to_pickle("./topoprocessed/" + input_filename.replace(".h5", "") + ".pkl")
+    for col in eta_cols:
+        df_pt_eta_phi_flipped.loc[events_to_flip,col] = -1.0*df_pt_eta_phi_flipped.loc[events_to_flip,col]
+
+    store.close()
+
+    del df_pt_eta_phi_rotated
+
+
+
+    # Format conversion
+    df_as_array = df_pt_eta_phi_flipped.to_numpy()
+    
+    # Extract events by excluding the final column. This final column includes the labels, which still need to be converted into one-hot target vectors.
+    data, labels = np.vsplit(df_as_array, [89])
+    labels = labels[0].astype(int)
+
+    # Extract target values and create one-hot arrays for each.
+    event_count = labels.shape[0]
+    target = np.zeros((event_count, 2))
+    target[np.arange(event_count), labels] = 1
+
+    # Saving
+    print("Saving.")
+
+    np.save(input_filename.replace(".h5", "_data.npy"), data)
+    np.save(input_filename.replace(".h5", "_target.npy"), target)

Helpers/fetch_and_prepare_datasets.py
@@ -10,9 +10,14 @@ import shutil
 from urllib.request import urlretrieve
 import tarfile
 from sklearn.datasets import fetch_openml
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
 import keras
 
 from ImageNet.compileDownload import compileDownload
+from TopoDNN.topodnnpreprocessing import topodnn_preprocess
 
 print(os.getcwd())
 
@@ -42,40 +47,31 @@ if (get_mnist):
     else:
         print("MNIST: Fetching Dataset...")
         (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+        # Scale data to range [0,1] instead of [0,255]
+        x_train = x_train.astype("float64")/255.0
+        x_test = x_test.astype("float64")/255.0
+
+        # Data format: add extra dimension to make each smaple (28,28,1) instead of (28,28)
+        x_train = np.expand_dims(x_train, -1)
+        x_test = np.expand_dims(x_test, -1)
+
+        # Create one-hot labels
+        target_train = np.zeros((x_train.shape[0], 10)).astype(np.int64)
+        target_train[np.arange(x_train.shape[0]), y_train.flatten()] = 1
+
+        target_test = np.zeros((x_test.shape[0], 10)).astype(np.int64)
+        target_test[np.arange(x_test.shape[0]), y_test.flatten()] = 1
+
         if not trainDataExists:     np.save("Datasets/MNIST/train_data.npy", x_train)
-        if not trainTargetExists:   np.save("Datasets/MNIST/train_target.npy", y_train)
+        if not trainTargetExists:   np.save("Datasets/MNIST/train_target.npy", target_train)
         if not testDataExists:      np.save("Datasets/MNIST/test_data.npy", x_test)
-        if not testTargetExists:    np.save("Datasets/MNIST/test_target.npy", y_test)
+        if not testTargetExists:    np.save("Datasets/MNIST/test_target.npy", target_test)
         print("MNIST: Completed.")
 
 # Fetch, save and extract the CIFAR-10 Dataset.
 # Originally fetched the CIFAR Dataset in batches - this is now commented out as it unnecessarily complex.
 # Keras handily provides the CIFAR-10 Dataset in preprocessed numpy array form. 
-'''
-if (get_cifar):
-    url = "https://www.cs.toronto.edu/%7Ekriz/cifar-10-python.tar.gz"
-    archive_path = "Datasets/CIFAR-10/cifar-10-python.tar.gz"
-
-    destination = "Datasets/CIFAR-10/"
-    folder = "batches"
-
-    if not os.path.isfile(archive_path):
-        print("CIFAR-10: Fetching archive.")
-        urlretrieve(url, archive_path)
-        print("Complete.")
-    
-    if (os.path.isdir(destination+folder)):
-        print("CIFAR-10: Clearing image folder.")
-        shutil.rmtree(destination+folder)
-        print("CIFAR-10: Complete.")
-
-    print("CIFAR-10: Extracting Archive.")
-    archive = tarfile.open(archive_path)
-    archive.extractall(destination)
-    os.rename(destination+"cifar-10-batches-py", destination+folder)
-    archive.close()
-    print("CIFAR-10: Complete.")
-'''
 print()
 if (get_cifar):
     trainDataExists = os.path.isfile("Datasets/CIFAR-10/train_data.npy")
@@ -88,10 +84,26 @@ if (get_cifar):
     else:
         print("CIFAR-10: Fetching Dataset...")
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+        # Scale data to range [0,1] instead of [0,255]
+        x_train = x_train.astype("float64")/255.0
+        x_test = x_test.astype("float64")/255.0
+
+        # Data format: add extra dimension to make each smaple (28,28,1) instead of (28,28)
+        x_train = np.expand_dims(x_train, -1)
+        x_test = np.expand_dims(x_test, -1)
+
+        # Create one-hot targets
+        target_train = np.zeros((x_train.shape[0], 10)).astype(np.int64)
+        target_train[np.arange(x_train.shape[0]), y_train.flatten()] = 1
+
+        target_test = np.zeros((x_test.shape[0], 10)).astype(np.int64)
+        target_test[np.arange(x_test.shape[0]), y_test.flatten()] = 1
+
         if not trainDataExists:     np.save("Datasets/CIFAR-10/train_data.npy", x_train)
-        if not trainTargetExists:   np.save("Datasets/CIFAR-10/train_target.npy", y_train)
+        if not trainTargetExists:   np.save("Datasets/CIFAR-10/train_target.npy", target_train)
         if not testDataExists:      np.save("Datasets/CIFAR-10/test_data.npy", x_test)
-        if not testTargetExists:    np.save("Datasets/CIFAR-10/test_target.npy", y_test)
+        if not testTargetExists:    np.save("Datasets/CIFAR-10/test_target.npy", target_test)
         print("CIFAR-10: Completed.")
 
 # Fetch, save and extract all three ImageNet v2 testing Datasets.
@@ -172,27 +184,33 @@ print()
 if (get_topodnn):
     destination = "Datasets/TopoDNN/"
     
-    url_test = "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=test.h5"
-    url_train = "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=train.h5"
-    url_validate = "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=val.h5"
+    # Data Fetching
 
-    if (os.path.isfile(destination+"test.h5")):
-        print("TopoDNN: Testing dataset found.")
-    else:
-        print("TopoDNN: Feching testing dataset.")
-        urlretrieve(url_test, destination+"test.h5")
-        print("TopoDNN: Complete.")
+    urls =      [
+                "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=test.h5",
+                "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=train.h5",
+                "https://syncandshare.desy.de/index.php/s/llbX3zpLhazgPJ6/download?path=%2F&files=val.h5"
+                ]
 
-    if (os.path.isfile(destination+"train.h5")):
-        print("TopoDNN: Training dataset found.")
-    else:
-        print("TopoDNN: Feching training dataset.")
-        urlretrieve(url_train, destination+"train.h5")
-        print("TopoDNN: Complete.")
+    names =     ["test", "train", "val"]
 
-    if (os.path.isfile(destination+"val.h5")):
-        print("TopoDNN: Validation dataset found.")
-    else:
-        print("TopoDNN: Feching validation dataset.")
-        urlretrieve(url_validate, destination+"val.h5")
-        print("TopoDNN: Complete.")
+    for i in range(len(urls)):
+        url = urls[i]
+        name = names[i]
+
+        # Fetching, if necessary
+        if (os.path.isfile(destination + name + ".h5")):
+            print("TopoDNN: \"" + name + "\" dataset found.")
+        else:
+            print("TopoDNN: Fetching \"" + name + "\" dataset...")
+            urlretrieve(url, destination + name + ".h5")
+            print("TopoDNN: Complete.")
+
+        # Preprocessing, if necessary
+        if (os.path.isfile(destination + name + "_data.npy") and os.path.isfile(destination + name + "_target.npy")):
+            print("TopoDNN: Preprocessed \"" + name + "\" dataset found.")
+        else:
+            print("TopoDNN: Preprocessing \"" + name + "\" dataset...")
+            topodnn_preprocess(destination + name + ".h5")
+        
+        print()