44821dc3e614661d93e2c68c141631d89d9cc66a
s17mmile <s17mmile@uni-bonn.de>
2025-07-29 17:02:19 +0200

    removed test.py

--- test.py
+++ test.py
@@ -14,40 +14,16 @@ import keras
 from Evaluation.dataset_analysis import *
 from Helpers.constrainers import *
 
-# def test(data):
-#     check = np.isnan(data)
-#     print(np.unique(check,return_counts=True))
-#     print(np.min(data))
-#     print(np.unravel_index(np.argmin(data), data.shape))
-#     print(np.max(data))
-#     print(np.unravel_index(np.argmax(data), data.shape))
-#     print()
+example = np.array([0,1,2,3,4,5,6,7,8,9])
 
-# def num_constits(jet):
-#     count = 30
-#     tol = 1e-12
-#     for i in range(0,90,3):
-#         if abs(jet[i]) < tol and abs(jet[i+1]) < tol and abs(jet[i+2]) < tol:
-#             count -= 1
-#     return count
+adversary = tf.convert_to_tensor(example)
 
-# def extract_pT(jet):
-#     return jet[0::3]
+example[:] += 2
 
-# data = np.load("Datasets/TopoDNN/train_data.npy", mmap_mode="r")
-# # test(data)
+print(example)
+print(adversary)
 
-# # adv1 = np.load("Adversaries/TopoDNN/conserveConstits_spreadLimit/PGD_train_data.npy", mmap_mode="r")
-# # test(adv1)
+adversary = adversary + 1
 
-# adv2 = np.load("Adversaries/TopoDNN/conserveConstits_spreadLimit_conserveGlobalEnergy/PGD_train_data.npy", mmap_mode="r")
-# # test(adv2)
-
-# adv3 = np.load("Adversaries/TopoDNN/conserveConstits_spreadLimit_conserveParticleEnergy/PGD_train_data.npy", mmap_mode="r")
-# # test(adv3)
-
-# for i in range(1024):
-#     print(i)
-#     print(num_constits(data[i]))
-#     print(num_constits(adv2[i]))
-#     print(num_constits(adv3[i]))
+print(example)
+print(adversary)
